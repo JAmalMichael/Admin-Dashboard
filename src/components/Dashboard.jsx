05f58bae10585bef90852ce2639ce531a34af905
JAmal Michael <eshimi090@gmail.com>
2024-06-21 13:13:36 -1200

    api, json-server package and Dashboard component.

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react'
+import './Styles/dashboard.css'
+
+function Dashboard() {
+  return (
+    <section className='dashboard section'>
+     <div className='row'>
+            <div className="col-lg-8">
+                <div className='row'>
+
+                </div>
+            </div>
+            <div className="col-lg-4"></div>
+     </div>
+    </section>
+
+  )
+}
+
+export default Dashboard
