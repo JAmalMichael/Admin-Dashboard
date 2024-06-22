ea9b801e38421356c24a81e57c8ce83f1d04c549
JAmal Michael <eshimi090@gmail.com>
2024-06-21 15:40:11 -1200

    Card and Card filter components

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,32 @@
 import React, {useState, useEffect} from 'react'
 import './Styles/dashboard.css'
+import Cards from './Cards';
 
 function Dashboard() {
+        const [cards, setCards] = useState({});
+
+        const fetchData = () => {
+            fetch('http://localhost:4000/cards')
+            .then(res => res.json())
+            .then(data => {
+                setCards(data)
+            })
+            .catch(e => console.log(e.message));
+        }
+
+        useEffect(() => {
+            fetchData();
+        }, [])
+
   return (
     <section className='dashboard section'>
      <div className='row'>
             <div className="col-lg-8">
                 <div className='row'>
-
+                    {
+                        cards && cards.length>0 &&
+                        cards.map(card => <Cards key={card._id} card={card}/>)
+                    }
                 </div>
             </div>
             <div className="col-lg-4"></div>
